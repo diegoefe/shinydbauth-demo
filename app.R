324e62393bb3efc16de2b555cc767438da59b124
Diego F(i7) <diegoefe@gmail.com>
2023-12-12 08:41:44 -0300

    Added auto-installation of required packages

--- app.R
+++ app.R
@@ -1,3 +1,11 @@
+options(warn=-1)
+# options(warn=0)
+if(!require("shinydbauth")) {
+   install.packages(readLines(file("required_packages.txt", "r")), repos='http://cran.us.r-project.org')
+} else {
+   print("Required packages already installed!")
+}
+
 library(shiny)
 library(shinydbauth)
 
